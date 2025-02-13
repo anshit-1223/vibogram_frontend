001d15da22a1e6c1552d1618233185c86df74f3d
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2025-02-13 20:19:27 +0530

    Add Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const loginImage = require('/assets/images/login.jpg');
 
   return (
     <>
-      <div className='border border-2'>
-        VIBOGRAM HOMEPAGE
+      <div className='row container-build'>
+        <div className='col-lg-2 col-md-2 col-sm-12 bg-success'></div>
+        <div className='col-lg-8 col-md-8 col-sm-12'>
+          <div className="row">
+            <div className='col-lg-6 bg-info'> <img src={loginImage} alt="LoginImage" /></div>
+            <div className='col-lg-6 bg-danger'>VIBOGRAM Login Page</div>
+          </div>
+        </div>
+        <div className='col-lg-2 col-md-2 col-sm-12 bg-primary'></div>
       </div>
     </>
   )
